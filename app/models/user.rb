1f58db2968a7d777bc2d4681bdcc3bb3c12c853a
Ken Vega <ken.vega.g@gmail.com>
2021-05-15 22:35:40 -0500

    Add schema comments for some models

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,3 +15,22 @@ class User < ApplicationRecord
   #   user&.authenticate(password)
   # end
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  email           :string
+#  name            :string
+#  password_digest :string
+#  phone           :string
+#  status          :integer
+#  token           :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_token  (token) UNIQUE
+#
