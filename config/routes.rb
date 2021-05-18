d88569ef1cfbbeac0ec1dd7dfd8da79e452e6b4a
Nick Correa <nickcorreagarcia@gmail.com>
2021-05-17 19:23:00 -0500

    Add regular store controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # get 'stores/show'
+  # get 'stores/create'
+  # get 'stores/update'
+  # get 'stores/destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :stores, only: %i[show create update destroy]
 end
